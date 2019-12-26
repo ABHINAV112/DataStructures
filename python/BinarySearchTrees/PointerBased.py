638e4ab7e6ab37525b6c8d613521ba3d3be01537
ABHINAV112 <nabhinav2000@gmail.com>
2019-12-26 16:06:56 +0530

    finished BST implementation

--- python/BinarySearchTrees/PointerBased.py
+++ python/BinarySearchTrees/PointerBased.py
@@ -9,14 +9,41 @@ class BST:
     def __init__(self):
         self.root = None
 
-    def insert_node(self,x):
-        new_node = Node(x)
-
-        def recurs(node,x):
-            pass
+    def insert_node(self.root,node): 
+        if root==None: 
+            root = node 
+        else: 
+            if root.val < node.val: 
+                if root.right == None: 
+                    root.right = node 
+                else: 
+                    insert(root.right, node) 
+            else: 
+                if root.left == None: 
+                    root.left = node 
+                else: 
+                    insert(root.left, node)
     
-    def delete_node(self,x):
-        pass
+    def delete_node(root, key): 
+        if root == None: 
+            return root  
+        if key < root.key: 
+            root.left = deleteNode(root.left, key) 
+        elif(key > root.key): 
+            root.right = deleteNode(root.right, key) 
+        else: 
+            if root.left == None : 
+                temp = root.right  
+                root = None 
+                return temp    
+            elif root.right == None : 
+                temp = root.left  
+                root = None
+                return temp 
+            temp = minValueNode(root.right) 
+            root.key = temp.key 
+            root.right = deleteNode(root.right , temp.key) 
+        return root 
 
     def in_order(self, node, callback):
         if(self.node!=None):
